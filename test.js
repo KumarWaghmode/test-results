ae8f15f8979361a7f48ad98ce1871cc65acc280c
Kumar Waghmode <KumarWaghmode@users.noreply.github.com>
2021-11-01 09:40:18 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 describe('team module', function() {
     
-    
+    beforeEach(() => {
+    jest.setTimeout(10000);
+  });
     it('should return members from mock', function( done ) {
         
       
