445212e8708da93aafcf3eb34f228c17f09f93a8
Kumar Waghmode <KumarWaghmode@users.noreply.github.com>
2021-11-01 09:05:25 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,11 @@
+var assert = require('chai').assert;
+
+describe('team module', function() {
+    
+    
+    it('should return members from mock', function( done ) {
+        
+       assert.equal( 10,10 );
+        
+    });
+});
