990186772da50708749dd9d02b32e2c54fc678ba
Kumar Waghmode <KumarWaghmode@users.noreply.github.com>
2021-11-01 09:19:33 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,9 @@
-var assert = require('chai').assert;
-
 describe('team module', function() {
     
     
     it('should return members from mock', function( done ) {
         
-       assert.equal( 10,10 );
+      
         
     });
 });
